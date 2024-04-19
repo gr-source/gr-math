1aed26ac8352e23aae5e006fcf2e047a46c4dd7f
darlissomls <darlissomls@gmail.com>
2024-04-19 00:19:53 -0300

    modified:   CMakeLists.txt 	renamed:    build/.cmake/api/v1/reply/cache-v2-b44609109e6209dbf998.json -> build/.cmake/api/v1/reply/cache-v2-8c66669bf7c6bdee9a49.json 	renamed:    build/.cmake/api/v1/reply/codemodel-v2-d1d289d70f6584e77a1f.json -> build/.cmake/api/v1/reply/codemodel-v2-7e7248d976755fd5a8b7.json 	new file:   build/.cmake/api/v1/reply/directory-.-Debug-59212fcab6a181510297.json 	deleted:    build/.cmake/api/v1/reply/directory-.-Debug-f5ebdc15457944623624.json 	renamed:    build/.cmake/api/v1/reply/index-2024-04-19T02-20-49-0486.json -> build/.cmake/api/v1/reply/index-2024-04-19T03-19-02-0762.json 	renamed:    build/.cmake/api/v1/reply/target-gr-math-Debug-91ad23c30d63937c06cc.json -> build/.cmake/api/v1/reply/target-gr-math-Debug-809dc29e85e90f7b793a.json 	modified:   build/.ninja_deps 	modified:   build/.ninja_log 	modified:   build/CMakeCache.txt 	modified:   build/CMakeFiles/CMakeOutput.log 	modified:   build/CMakeFiles/TargetDirectories.txt 	modified:   build/Testing/20240419-0217/Test.xml 	modified:   build/Testing/Temporary/LastTest_20240419-0217.log 	modified:   build/build.ninja 	modified:   build/cmake_install.cmake 	new file:   build/install_manifest.txt

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/usr/lib")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,6 +40,53 @@ endif()
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so"
+         RPATH "")
+  endif()
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/lib/gr-math/libgr-math.so")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/lib/gr-math" TYPE SHARED_LIBRARY FILES "/home/grsource/Documentos/AppProjects/gr-math/lib/libgr-math.so")
+  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/usr/local/lib/gr-math/libgr-math.so")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/usr/local/include/gr-math/math.h;/usr/local/include/gr-math/vector2.h;/usr/local/include/gr-math/vector3.h;/usr/local/include/gr-math/vector4.h;/usr/local/include/gr-math/matrix3x3.h;/usr/local/include/gr-math/matrix4x4.h;/usr/local/include/gr-math/quaternion.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "/usr/local/include/gr-math" TYPE FILE FILES
+    "/home/grsource/Documentos/AppProjects/gr-math/src/math.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/vector2.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/vector3.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/vector4.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/matrix3x3.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/matrix4x4.h"
+    "/home/grsource/Documentos/AppProjects/gr-math/src/quaternion.h"
+    )
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
