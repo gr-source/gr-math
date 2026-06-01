af1fda7fbd37b6b42fb56a5006ff393c85444c1c
gr-source <darlissomlsa@gmail.com>
2026-06-01 13:53:21 -0300

    git ignore

--- include/gr-math/matrix3x3.hpp
+++ include/gr-math/matrix3x3.hpp
@@ -48,47 +48,5 @@ inline mat<f32, 3, 3> mat<f32, 3, 3>::zeroMatrix = {
     vector<f32, 3>{0.0f, 0.0f, 0.0f}
 };
 
-/*
-#include "vector3.hpp"
-#include "types.hpp"
-
-/* === Matrix3x3===
-* ordem de multiplicação:
-* m = t * r * s
-
-struct Matrix3x3 {
-    union {
-        struct {
-            Vector3 row0;
-            Vector3 row1;
-            Vector3 row2;
-        };
-        Vector3 data[3];
-    };
-
-    Matrix3x3(const Vector3& row0, const Vector3& row1, const Vector3& row2);
-
-    Matrix3x3(const Matrix4x4& rhs);
- 
-    const Matrix3x3 transpose() const;
-
-    const Vector3 getColumn(int index) const;
-
-    const Vector3 getRow(int index) const;
-
-    const Vector3 &operator [](int index) const;
-
-    Vector3 &operator [](int index);
-
-    static const Matrix3x3 identityMatrix;
-    static const Matrix3x3 zeroMatrix;
-};
-
-const Matrix3x3 operator*(const Matrix3x3& lhs, const Matrix3x3& rhs) noexcept;
-
-const Vector3 operator*(const Matrix3x3& lhs, const Vector3& rhs) noexcept;
-
-const Vector3 operator*(const Vector3& lhs, const Matrix3x3& rhs) noexcept;
-*/
 
 
